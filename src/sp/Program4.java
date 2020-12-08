5caf09dc27f51144516afd631e7c5aaf62e2a53d
Vikas <Vikas@DESKTOP-1MN65KS>
2020-12-08 22:46:33 +0530

    [vikas]:star program is uploaded

---
+++ src/sp/Program4.java
@@ -0,0 +1,37 @@
+package sp;
+
+public class Program4
+ {
+  public static void main(String[] args)
+  {
+	//  *
+	// **
+	//***  
+	  
+	  int space =2;
+	  int star=1;
+	  
+	  for (int i = 1; i <=3; i++) 
+	  {
+		 
+		  for (int j = 1; j <=space; j++)
+		  {
+			System.out.print(" ");
+		  }
+		  
+		  for (int k = 1; k <=star; k++) 
+		  {
+			System.out.print("*");
+		  }
+		  
+		  System.out.println();
+		  
+		  space--;
+		  
+		  star++;
+		  
+		
+	  }
+  }
+	
+ }
